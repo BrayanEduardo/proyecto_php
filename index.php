5e5ba64fd8b9bf2769b72ffbc73e355c3c180fe4
BrayanEduardo <bf377954@gmail.com>
2020-05-16 16:16:45 -0600

    Terminado el proyecto

--- index.php
+++ index.php
@@ -7,9 +7,14 @@
 </head>
 <body>
     
-    <center><h1>Hola Mundo</h1></center>
-  <a href="ejemplo1.php">Calcular Salario</a>
-  <a href="Nuevo.php">NUEVO</a>
+    <center><h1>Bienvenidos</h1></center>
+    <center>
+ <button><a href="ejemplo1.php">Calcular Salario</button> 
+ <br>
+ <br>
+ <br>
+ <button><a href="Nuevo.php">NUEVO</a></button>
+ </center> 
 </body>
 
 </html>
